8c49bb59654fd4c39387d7650799ca165e292c85
Janick Hurschler <janickhurschler06@gmail.com>
2025-03-13 18:47:32 +0100

    Ändere Hintergrundfarbe des Headers in der Notizen-App auf ein dunkleres Blau für ein verbessertes visuelles Design

--- notizen-app/src/components/Header.tsx
+++ notizen-app/src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC = () => {
 
   return (
     <header className=" shadow">
-      <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="bg-[#08192A] max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
